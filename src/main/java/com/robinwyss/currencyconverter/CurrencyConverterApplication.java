dafdea5d135cb1b62f6d3042ba89b643d89a00bf
Robin Wyss <me@robinwyss.com>
2022-04-20 14:47:20 +0200

    added servlet initializer

--- src/main/java/com/robinwyss/currencyconverter/CurrencyConverterApplication.java
+++ src/main/java/com/robinwyss/currencyconverter/CurrencyConverterApplication.java
@@ -2,12 +2,19 @@ package com.robinwyss.currencyconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurrencyConverterApplication {
+public class CurrencyConverterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CurrencyConverterApplication.class);
 	}
 
 }
